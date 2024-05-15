bc4ab849dea35af56e1852aef6c5ecf98f445138
Philippe Duchesne <pduchesne@gmail.com>
2024-05-15 21:01:54 +0200

    add dark theme error handling

--- src/monaco.ttl.ts
+++ src/monaco.ttl.ts
@@ -1,7 +1,7 @@
 import * as monaco from "monaco-editor";
 import IMonarchLanguage = monaco.languages.IMonarchLanguage;
-import { WorkerManager } from "./WorkerManager";
-import DiagnosticsAdapter, { WorkerAccessor } from "./DiagnosticsAdapter";
+//import { WorkerManager } from "./WorkerManager";
+//import DiagnosticsAdapter, { WorkerAccessor } from "./DiagnosticsAdapter";
 import {CancellationToken, editor} from "monaco-editor";
 import * as rdflib from 'rdflib';
 import { NamedNode } from "rdflib";
@@ -23,10 +23,31 @@ const turtleTheme: IStandaloneThemeData = {
     { token: 'string', foreground: '#2a6c00' },
     { token: 'comment', foreground: '#636363', fontStyle: 'italic' },
     { token: 'operator', foreground: '#b86d04', fontStyle: 'bold' },
-
   ],
   colors: {
     'editor.foreground': '#000000'
+  }
+};
+
+const turtleThemeDark: IStandaloneThemeData = {
+  base: 'vs',
+  inherit: false,
+  rules: [
+    { token: 'iri', foreground: '#3383ff' },
+    { token: 'keyword', foreground: '#68a7eb', fontStyle: 'bold' },
+    { token: 'prefix', foreground: '#ad57e7' },
+    { token: 'prefixedname', foreground: '#998f33' },
+    { token: 'symbol', foreground: '#939393' },
+    { token: 'invalid', foreground: '#c91414' },
+    { token: 'string', foreground: '#4e9f1b' },
+    { token: 'comment', foreground: '#a6a6a6', fontStyle: 'italic' },
+    { token: 'operator', foreground: '#cd8a2c', fontStyle: 'bold' },
+  ],
+  colors: {
+    // active line number : #d9e1ff
+    // .codicon-folding-expanded : #b5b5b5
+    'editor.foreground': '#cfcfcf',
+    'editor.background': '#191919'
   }
 };
 
@@ -149,136 +170,144 @@ const linkProvider: monaco.languages.LinkProvider = {
 
 const createHoverProvider = (monacoInstance: typeof monaco): monaco.languages.HoverProvider => ({
   provideHover: function (model, position, token: CancellationToken) {
-    //var word = model.getWordAtPosition(position);
+    try {
+      //var word = model.getWordAtPosition(position);
 
-    const rdfGraph: rdflib.Store = (model as any).rdfGraph;
+      const rdfGraph: rdflib.Store = (model as any).rdfGraph;
 
-    //let textUntilPosition = model.getValueInRange({startLineNumber: 1, startColumn: 1, endLineNumber: position.lineNumber, endColumn: position.column})
-    let lineContent = model.getLineContent(position.lineNumber);
-    const lineTokens = monacoInstance.editor.tokenize(lineContent, 'turtle')[0];
+      //let textUntilPosition = model.getValueInRange({startLineNumber: 1, startColumn: 1, endLineNumber: position.lineNumber, endColumn: position.column})
+      let lineContent = model.getLineContent(position.lineNumber);
+      const lineTokens = monacoInstance.editor.tokenize(lineContent, 'turtle')[0];
 
-    //const lineTokens = (model as any).getLineTokens(position.lineNumber);
+      //const lineTokens = (model as any).getLineTokens(position.lineNumber);
 
-    const hoveredTokenIdx = lineTokens.findIndex( (t, idx) => {
-      return t.offset <= position.column-1 && (idx >= lineTokens.length-1 || position.column-1 < lineTokens[idx+1].offset)
-    });
-    const hoveredTokenStartOffset = lineTokens[hoveredTokenIdx].offset;
-    const hoveredTokenEndOffset = lineTokens[hoveredTokenIdx+1] ? lineTokens[hoveredTokenIdx+1].offset : lineContent.length;
-    const hoveredToken = lineTokens[hoveredTokenIdx];
+      const hoveredTokenIdx = lineTokens.findIndex((t, idx) => {
+        return t.offset <= position.column - 1 && (idx >= lineTokens.length - 1 || position.column - 1 < lineTokens[idx + 1].offset)
+      });
+      const hoveredTokenStartOffset = lineTokens[hoveredTokenIdx].offset;
+      const hoveredTokenEndOffset = lineTokens[hoveredTokenIdx + 1] ? lineTokens[hoveredTokenIdx + 1].offset : lineContent.length;
+      const hoveredToken = lineTokens[hoveredTokenIdx];
 
 
-    const tokenText = lineContent.substring(hoveredTokenStartOffset, hoveredTokenEndOffset)
+      const tokenText = lineContent.substring(hoveredTokenStartOffset, hoveredTokenEndOffset)
 
-    var range = {
-      startLineNumber: position.lineNumber,
-      endLineNumber: position.lineNumber,
-      startColumn: hoveredTokenStartOffset + 1,
-      endColumn: hoveredTokenEndOffset + 1
-    };
+      var range = {
+        startLineNumber: position.lineNumber,
+        endLineNumber: position.lineNumber,
+        startColumn: hoveredTokenStartOffset + 1,
+        endColumn: hoveredTokenEndOffset + 1
+      };
 
-    const contents: monaco.IMarkdownString[] = [];
+      const contents: monaco.IMarkdownString[] = [];
 
-    if (hoveredToken.type == "prefix.turtle" ) {
-      return null;
-      /*
-      contents.push(
-        { value: `## ${tokenText}` },
-        { value: `${rdfGraph.namespaces[tokenText.split(':')[0]]}` },
-      )
-
-       */
-    }
-    else if (hoveredToken.type == "prefixedname.turtle" || hoveredToken.type == "iri.turtle") {
-
-      let iri: string;
-      if (hoveredToken.type == "prefixedname.turtle") {
-        const [prefix, name] = tokenText.split(':');
-        iri = rdfGraph.namespaces[prefix]+name;
-      } else {
-        iri = tokenText.substring(1, tokenText.length-1);
-      }
-
-      const name = rdfGraph.anyValue(rdflib.namedNode(iri), SDO('name')) || rdfGraph.anyValue(rdflib.namedNode(iri), SDO('title'));
-      const types = [...rdfGraph.each(rdflib.namedNode(iri), RDFS('type')), ...rdfGraph.each(rdflib.namedNode(iri), SDO('additionalType'))]
-
-      const typeNames = types.map(n => n.value);
-
-      contents.push(
-          { value: `## ${name || iri}` },
-          { value: `${typeNames.join(", ")}` },
-          //{ value: `${tokenText}` }
-      )
-
-      // TODO fix Recommandation into Recommendation everywhere
-      if (typeNames.includes(DV('RecommandationExplain').value) || typeNames.includes(SDO('Recommandation').value) || typeNames.includes(SDO('Recommendation').value)) {
-        const rank = rdfGraph.anyValue(rdflib.namedNode(iri), DV('rank'));
-        const weight = rdfGraph.anyValue(rdflib.namedNode(iri), DV('weight'));
-        contents.push(
-            { value: `Rank **${rank}**` },
-            { value: `Weight **${weight}**` }
-        )
-      } if (typeNames.includes(SDO('Action').value)) {
-        const object = rdfGraph.any(rdflib.namedNode(iri), SDO('object'));
-        const objectName = object && rdfGraph.anyValue(object as NamedNode, SDO('name'));
-        contents.push(
-            { value: `Action on **${objectName}**` },
-        )
-      } else if (typeNames.includes(GConsent('Consent').value)) {
-        const purpose = rdfGraph.anyValue(rdflib.namedNode(iri), GConsent('purpose'));
-        contents.push(
-            { value: `**${purpose}**` },
-        )
-      } else if (typeNames.includes(SDO('MusicEvent').value)) {
-        const startDate = rdfGraph.anyValue(rdflib.namedNode(iri), SDO('startDate'));
-        const performer = rdfGraph.any(rdflib.namedNode(iri), SDO('performer'));
-        const performerName = performer && rdfGraph.anyValue(performer as NamedNode, SDO('name'));
-        const location = rdfGraph.any(rdflib.namedNode(iri), SDO('location'));
-        const locationName = location && rdfGraph.anyValue(location as NamedNode, SDO('name'));
-        contents.push(
-            { value: `Artist **${performerName}**` },
-            { value: `Location **${locationName}**` },
-        )
-        if (startDate)
-          contents.push(
-              { value: `Date **${new Date(startDate).toLocaleString()}**` },
-          )
-      } else if (typeNames.includes(SDO('MusicAlbum').value)) {
-
+      if (hoveredToken.type == "prefix.turtle") {
+        return null;
         /*
-        const query = rdflib.SPARQLToQuery(`SELECT ?artist WHERE { <${iri}> <${SDO('byArtist').value}>/<${SDO('name').value}> ?artist }.`, false, rdfGraph);
-        const result$ = new Promise<Term | null>( (resolve, reject) => {
-          query && rdfGraph.query(query, bindings => {
-            resolve(bindings['artist'])
-            // TODO what if rejected ?
-          });
-        })
-        const artist = await result$;
-        */
-        const artist = rdfGraph.any(rdflib.namedNode(iri), SDO('byArtist'));
-        const artistName = artist && rdfGraph.anyValue(artist as NamedNode, SDO('name'));
+        contents.push(
+          { value: `## ${tokenText}` },
+          { value: `${rdfGraph.namespaces[tokenText.split(':')[0]]}` },
+        )
+
+         */
+      } else if (hoveredToken.type == "prefixedname.turtle" || hoveredToken.type == "iri.turtle") {
+
+        let iri: string;
+        if (hoveredToken.type == "prefixedname.turtle") {
+          const [prefix, name] = tokenText.split(':');
+          iri = rdfGraph.namespaces[prefix] + name;
+        } else {
+          iri = tokenText.substring(1, tokenText.length - 1);
+        }
+
+        const name = rdfGraph.anyValue(rdflib.namedNode(iri), SDO('name')) || rdfGraph.anyValue(rdflib.namedNode(iri), SDO('title'));
+        const types = [...rdfGraph.each(rdflib.namedNode(iri), RDFS('type')), ...rdfGraph.each(rdflib.namedNode(iri), SDO('additionalType'))]
+
+        const typeNames = types.map(n => n.value);
 
         contents.push(
-            { value: `by **${artistName}**` },
+            {value: `## ${name || iri}`},
+            {value: `${typeNames.join(", ")}`},
+            //{ value: `${tokenText}` }
         )
+
+        // TODO fix Recommandation into Recommendation everywhere
+        if (typeNames.includes(DV('RecommandationExplain').value) || typeNames.includes(SDO('Recommandation').value) || typeNames.includes(SDO('Recommendation').value)) {
+          const rank = rdfGraph.anyValue(rdflib.namedNode(iri), DV('rank'));
+          const weight = rdfGraph.anyValue(rdflib.namedNode(iri), DV('weight'));
+          contents.push(
+              {value: `Rank **${rank}**`},
+              {value: `Weight **${weight}**`}
+          )
+        }
+        if (typeNames.includes(SDO('Action').value)) {
+          const object = rdfGraph.any(rdflib.namedNode(iri), SDO('object'));
+          const objectName = object && rdfGraph.anyValue(object as NamedNode, SDO('name'));
+          contents.push(
+              {value: `Action on **${objectName}**`},
+          )
+        } else if (typeNames.includes(GConsent('Consent').value)) {
+          const purpose = rdfGraph.anyValue(rdflib.namedNode(iri), GConsent('purpose'));
+          contents.push(
+              {value: `**${purpose}**`},
+          )
+        } else if (typeNames.includes(SDO('MusicEvent').value)) {
+          const startDate = rdfGraph.anyValue(rdflib.namedNode(iri), SDO('startDate'));
+          const performer = rdfGraph.any(rdflib.namedNode(iri), SDO('performer'));
+          const performerName = performer && rdfGraph.anyValue(performer as NamedNode, SDO('name'));
+          const location = rdfGraph.any(rdflib.namedNode(iri), SDO('location'));
+          const locationName = location && rdfGraph.anyValue(location as NamedNode, SDO('name'));
+          contents.push(
+              {value: `Artist **${performerName}**`},
+              {value: `Location **${locationName}**`},
+          )
+          if (startDate)
+            contents.push(
+                {value: `Date **${new Date(startDate).toLocaleString()}**`},
+            )
+        } else if (typeNames.includes(SDO('MusicAlbum').value)) {
+
+          /*
+          const query = rdflib.SPARQLToQuery(`SELECT ?artist WHERE { <${iri}> <${SDO('byArtist').value}>/<${SDO('name').value}> ?artist }.`, false, rdfGraph);
+          const result$ = new Promise<Term | null>( (resolve, reject) => {
+            query && rdfGraph.query(query, bindings => {
+              resolve(bindings['artist'])
+              // TODO what if rejected ?
+            });
+          })
+          const artist = await result$;
+          */
+          const artist = rdfGraph.any(rdflib.namedNode(iri), SDO('byArtist'));
+          const artistName = artist && rdfGraph.anyValue(artist as NamedNode, SDO('name'));
+
+          contents.push(
+              {value: `by **${artistName}**`},
+          )
+        } else {
+          // if unknown type and no name to display, skip the popup
+          if (!name) return null;
+        }
+
       } else {
-        // if unknown type and no name to display, skip the popup
-        if (!name) return null;
+        /*
+        contents.push(
+          { value: `${hoveredToken.type}` },
+          { value: `${tokenText}` }
+        )
+
+         */
+        return null;
       }
 
-    } else {
-      /*
-      contents.push(
-        { value: `${hoveredToken.type}` },
-        { value: `${tokenText}` }
-      )
+      return {
+        range,
+        contents
+      }
+    } catch (err) {
+      // catch (parsing) errors
+      console.warn("Turtle hover failed :");
+      console.warn(err);
 
-       */
       return null;
-    }
-
-    return {
-      range,
-      contents
     }
   }
 })
@@ -299,6 +328,8 @@ export const onTurtleCb = (monacoInstance: typeof monaco) => () => {
     ],
     comments: {lineComment: '#'}
   });
+
+  /* This causes errors when changing the content. Why was this necessary ?
   const client = new WorkerManager();
 
   const worker: WorkerAccessor = (...uris) => {
@@ -306,6 +337,8 @@ export const onTurtleCb = (monacoInstance: typeof monaco) => () => {
   };
   //Call the errors provider
   new DiagnosticsAdapter(worker);
+
+   */
 };
 
 /*
@@ -320,6 +353,7 @@ monaco.languages.registerTokensProviderFactory('turtle', {
 export function registerTurtle(monacoInstance: typeof monaco) {
   if (!monacoInstance.languages.getLanguages().some(({ id }) => id === 'turtle')) {
     monacoInstance.editor.defineTheme('turtleTheme', turtleTheme);
+    monacoInstance.editor.defineTheme('turtleThemeDark', turtleThemeDark);
 
     // register file types and extension for a languageId
     monacoInstance.languages.register({
